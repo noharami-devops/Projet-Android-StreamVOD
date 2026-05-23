893af442f9ce5c73169fd9895456f09823a668c5
RAMI Noha <noha.rami@etu.uae.ac.ma>
2026-05-23 13:30:34 +0100

    feat: initial structure + theme + auth screens

---
+++ app/src/main/java/com/groupe9/streamvod/StreamVodApp.kt
@@ -0,0 +1,7 @@
+package com.groupe9.streamvod
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class StreamVodApp : Application()
---
+++ app/src/main/java/com/groupe9/streamvod/ui/navigation/AppNavigation.kt
@@ -0,0 +1,31 @@
+package com.groupe9.streamvod.ui.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.groupe9.streamvod.ui.auth.LoginScreen
+import com.groupe9.streamvod.ui.auth.RegisterScreen
+
+@Composable
+fun AppNavigation() {
+    val navController = rememberNavController()
+
+    NavHost(
+        navController = navController,
+        startDestination = "login"
+    ) {
+        composable("login") {
+            LoginScreen(
+                onLoginSuccess = { navController.navigate("home") },
+                onNavigateToRegister = { navController.navigate("register") }
+            )
+        }
+        composable("register") {
+            RegisterScreen(
+                onRegisterSuccess = { navController.navigate("login") },
+                onNavigateToLogin = { navController.popBackStack() }
+            )
+        }
+    }
+}
